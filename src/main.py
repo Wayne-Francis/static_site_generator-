5a2dc055d2d6f28bf97057bd17422f5f329bbaf9
MrFaultyT <wanye787@gmail.com>
2025-09-04 20:39:12 +0100

    just for my streak I know its cheating but dnd tonight :(

--- src/main.py
+++ src/main.py
@@ -2,6 +2,9 @@ print("hello world")
 
 from textnode import *
 
+def copy_contents():
+    this is to save my streak
+
 def main():
     textnode = TextNode("This is some text", TextType.LINK, "https://www.boot.dev")
     print(textnode)
