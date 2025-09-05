d00c9a98f5439bc589ba47b77e3251128ecf2286
MrFaultyT <wanye787@gmail.com>
2025-09-05 18:30:48 +0100

    started the delete and copy function

--- src/main.py
+++ src/main.py
@@ -1,9 +1,17 @@
 print("hello world")
 
+import os
 from textnode import *
 
-def copy_contents():
-    this is to save my streak
+def copy_contents(public_directory,static_directory, file_path):
+
+    abs_public = os.path.abspath(public_directory) # creates the abs path to the work
+    abs_file_path = os.path.abspath(os.path.join(abs_public, file_path)) # this could be useful for later 
+    public_path_exists = os.path.exists(abs_public) # creates a boolean varable to determine the public directory exists 
+    if public_path_exists:
+        
+
+
 
 def main():
     textnode = TextNode("This is some text", TextType.LINK, "https://www.boot.dev")
